da44ddcec5c571802167087f51d839a2f0575292
micheal <mojemoron2009@yahoo.com>
2019-11-02 09:19:16 +0100

    fix heroku release error 2

--- heroku-container-release.sh
+++ heroku-container-release.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 imageId=$(docker inspect registry.heroku.com/teamworka/web --format={{.Id}})
 payload='{"updates":[{"type":"web","docker_image":"'"$imageId"'"}]}'
-curl -n -X PATCH https://api.heroku.com/apps/teamworks/formation \
+curl -n -X PATCH https://api.heroku.com/apps/teamworka/formation \
 -d "$payload" \
 -H "Content-Type: application/json" \
 -H "Accept: application/vnd.heroku+json; version=3.docker-releases" \
