75b676fcd6c13ac53e40fd71b7f1e3c3f1c8480b
micheal <mojemoron2009@yahoo.com>
2019-11-02 12:33:03 +0100

    add api test

---
+++ app/frontend/src/components/App.jsx
@@ -0,0 +1,19 @@
+import React, {Component, Fragment} from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends Component {
+
+    render() {
+        return (
+
+            <div className="container">
+                <h1>Welcome</h1>
+            </div>
+
+
+        )
+    }
+}
+
+
+ReactDOM.render(<App/>, document.getElementById('app'));
--- app/teamwork/settings_dev.py
+++ app/teamwork/settings_dev.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api',
+    'frontend',
 ]
 
 MIDDLEWARE = [
@@ -120,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
