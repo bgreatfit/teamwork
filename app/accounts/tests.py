57fff6e62362fe72dec09fb82585b28d3c893c46
micheal <mojemoron2009@yahoo.com>
2019-11-08 06:33:52 +0100

    chores(test) add test for response data

--- app/accounts/tests.py
+++ app/accounts/tests.py
@@ -1,24 +1,31 @@
+import json
 from django.test import TestCase
 # Add these imports at the top
 from django.urls import reverse
 from rest_framework.test import APIClient
+from rest_framework.test import APITestCase
 from rest_framework import status
+
 
 # Create your tests here.
 # Define this after the ModelTestCase
-class ViewTestCase(TestCase):
+class AdminRegistrationAPIViewTestCase(APITestCase):
     """Test suite for the api views."""
 
-    def setUp(self):
-        """Define the test client and other test variables."""
-        self.client = APIClient()
-        self.account_data = {'username': 'Mike', 'email': 'mioshine@g.com',
-                                'password': '1234567'}
-        self.response = self.client.post(
-            reverse('create-user'),
-            self.account_data,
-            format="json")
-
     def test_admin_can_create_a_employee(self):
+        """
+        test that admin can create employee
+        :param self:
+        :return:
+        """
+
+        account_data = {"username": "Mike", "email": "mioshine@g.com",
+                             "password": "1234567"}
+        response = self.client.post(
+            reverse('create-user'),
+            account_data,
+            format="json")
         """Test the api has bucket creation capability."""
-        self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        print(response.content)
+        self.assertTrue("data" in json.loads(response.content))
