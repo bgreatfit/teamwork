f4bc507cb9dd508bdc1aa589d06d1d4004bf4bb5
micheal <mojemoron2009@yahoo.com>
2019-11-10 05:03:33 +0100

    features(create-user) modify api response

--- app/accounts/models.py
+++ app/accounts/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
 
 # Create your models here.
+class User(AbstractUser):
+    username = models.CharField(max_length=50, help_text="enter username")
+    email = models.EmailField(_('email address'), unique=True)
+    #
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'password']
